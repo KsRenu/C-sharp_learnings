3145a3a4579341ecbbbc05128c0e8a5354dda0c4
Renu Konda <renuks0802@gmail.com>
2022-11-06 08:23:09 +0530

    updating

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -7,11 +7,29 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
+
+
+            // Console.WriteLine("What is your name?");
+            // var name = Console.ReadLine();
+            // var currentDate = DateTime.Now;
+            // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            // Console.Write($"{Environment.NewLine}Press any key to exit...");
+
+            // var vare1="1234";
+            // byte vare2=Convert.ToByte(vare1); This actually crashes the program So use try and catch
+            // int vare2=Convert.ToInt32(vare1);
+            // Console.WriteLine(vare2);
+            // Console.ReadKey(true);
+            
+            try{
+                var vare1="1234";
+                byte vare2=Convert.ToByte(vare1); 
+            }
+            catch(Exception){
+                Console.WriteLine("Ther is some error in the code.");
+            }
+
+            
             Console.ReadKey(true);
         }
     }
