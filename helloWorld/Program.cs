65a1f1587fae1c36b958e1d6682294575607b2f7
Renu Konda <renuks0802@gmail.com>
2022-12-06 03:15:44 +0530

    learnings

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -68,7 +68,72 @@ namespace HelloWorld
         // Console.WriteLine($"Hello {name.ToUpper()}!");
 
 
+        //prints 77
+        // string firstName = "Bob";
+        // int widgetsSold = 7;
+        // Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
 
+        //prints 14
+        // string firstName = "Bob";
+        // int widgetsSold = 7;
+        // Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+        //int sum = 7 + 5;
+        // int difference = 7 - 5;
+        // int product = 7 * 5;
+        // int quotient = 7 / 5;
+        // decimal decimalQuotient = 7.0m / 5;
+
+
+        // decimal decimalQuotient = 7 / 5.0m;
+        // decimal decimalQuotient = 7.0m / 5.0m;
+
+        // //to get input only in int and need to produce the deci result
+        // int first = 7;
+        // int second = 5;
+        // decimal quotient = (decimal)first / (decimal)second;
+
+        // //to get remainders
+        // Console.WriteLine("Modulus of 200 / 5 : " + (200 % 5));
+        // Console.WriteLine("Modulus of 7 / 5 : " + (7 % 5));
+
+        // Console.WriteLine("Sum: " + sum);
+        // Console.WriteLine("Difference: " + difference);
+        // Console.WriteLine("Product: " + product);
+        // Console.WriteLine("Quotient: " + quotient);
+        // Console.WriteLine("Decimal quotient: " + decimalQuotient);
+
+
+        //operators
+        // int value = 1;
+
+        // value = value + 1;
+        // Console.WriteLine("First increment: " + value);
+
+        // value += 1;
+        // Console.WriteLine("Second increment: " + value);
+
+        // value++;
+        // Console.WriteLine("Third increment: " + value);
+
+        // value = value - 1;
+        // Console.WriteLine("First decrement: " + value);
+
+        // value -= 1;
+        // Console.WriteLine("Second decrement: " + value);
+
+        // value--;
+        // Console.WriteLine("Third decrement: " + value);
+
+
+
+
+        //int value = 1;
+        // value++;
+        // Console.WriteLine("First: " + value);
+        // Console.WriteLine("Second: " + value++);
+        // Console.WriteLine("Third: " + value);
+        // Console.WriteLine("Fourth: " + (++value));
         }
     }
 }
