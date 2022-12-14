efba29c8e29a2a2f2ecf0d8d5a1a9b885ce5bad5
Renu Konda <renuks0802@gmail.com>
2022-12-14 14:41:16 +0530

    methods_overloading

---
+++ WorkoutProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Question: Use a method of the System.Math class to determine which of two numbers is larger.
+
+int val1=500;
+int val2=600;
+Console.WriteLine( System.Math.Max (val1, val2));
+
+//solution
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
