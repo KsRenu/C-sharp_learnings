ca1f5c0069d798a838c725685a44d25a2bc3520a
Renu Konda <renuks0802@gmail.com>
2023-01-07 09:10:58 +0530

    stringModifications

--- stringModifications/Program.cs
+++ stringModifications/Program.cs
@@ -1,2 +1,117 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//Write code to find an opening and closing parenthesis embedded in a string
+string message = "Find what is (inside the parentheses)";
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
+
+//retrieve the value between two parenthesis characters
+string message = "Find what is (inside the parentheses)";
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+
+//Avoid magic values
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+
+//Write code to retrieve the last occurrence of a sub string
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+
+//Update the code example to work with different types of symbol sets
+string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+char[] openSymbols = { '[', '{', '(' };
+
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+    if (openingPosition == -1) break;
+
+    string currentSymbol = message.Substring(openingPosition, 1);
+  char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
+
+
+//challenge
+///Given the starting point in the following code listing, you'll add code to extract, replace, and remove portions of the input's value to produce the desired output.
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+string quantity = "";
+string output = "";
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+
+//desired Output is:
+// Output: <h2>Widgets &reg;</h2><span>5000</span>
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+const string spanTag = "<span>";
+
+// Extract the quantity
+int quantityStart = input.IndexOf(spanTag);
+int quantityEnd = input.IndexOf("</span>");
+quantityStart += spanTag.Length;
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+output = input.Replace("&trade;", "&reg;");
+
+// Remove the opening <div> tag
+int divStart = input.IndexOf("<div");
+int divEnd = input.IndexOf(">");
+int divLength = divEnd - divStart;
+divLength += 1;
+output = output.Remove(divStart, divLength);
+
+// Remove the closing <div> tag
+int divCloseStart = output.IndexOf("</div");
+int divCloseEnd = output.IndexOf(">", divCloseStart);
+int divCloseLength = divCloseEnd - divCloseStart;
+divCloseLength += 1;
+output = output.Remove(divCloseStart, divCloseLength);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
