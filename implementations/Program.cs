d8d87dd2030102636d2e467dc72fdb2a69a02abb
Renu Konda <renuks0802@gmail.com>
2023-01-08 16:17:18 +0530

    dtypes, operators, string operations

--- implementations/Program.cs
+++ implementations/Program.cs
@@ -1,90 +1,90 @@
 ﻿
-// //hello world
-// using System;
-// namespace implementations
-// {
-//     class implementations{
-//         static void Main(string[] args){
-//             Console.WriteLine("Hello, World!");
-//             Console.WriteLine(3 + 3);
+//hello world
+using System;
+namespace implementations
+{
+    class implementations{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(3 + 3);
 
-//             //variables
-//             int myNum = 5;
-//             float myNum1 = 5.75F;
-//             double myDoubleNum = 5.99D;
-//             char myLetter = 'D';
-//             bool myBool = true;
-//             string myText = "Hello";
-//             string myText1 = "Hello";
-//             string merged = myText+myText1;
-//             const int constant = 15;
-//             float f1 = 35e3F;
-//             double d1 = 12E4D;
-//             Console.WriteLine("The intiated variables are :\tint = " + myNum +"\tFloat = " + myNum1 + "\tdouble = "+ myDoubleNum + "\tchar = " + myLetter + "\tbool = "+ myBool + "\tString = "+ myText + "\tConstant = "+ constant);
-//             Console.WriteLine("Merged text is : " + merged);
-//             Console.WriteLine("The e value conversion be like: \n Float 35e3F = "+ f1 + "\n Double 12E4D = "+d1);
-
-
-//             //type casting
-//             int n1=1;
-//             double d2= n1;
-//             int n2= (int)d2;//explicit conversion;
-//             //string s1="hi";
-//             Console.WriteLine(d2);//implicit conversion;;
-//             Console.WriteLine(n2);
-//             Console.WriteLine(Convert.ToString(n1));
-//             Console.WriteLine(Convert.ToInt32(d2));
+            //variables
+            int myNum = 5;
+            float myNum1 = 5.75F;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+            string myText1 = "Hello";
+            string merged = myText+myText1;
+            const int constant = 15;
+            float f1 = 35e3F;
+            double d1 = 12E4D;
+            Console.WriteLine("The intiated variables are :\tint = " + myNum +"\tFloat = " + myNum1 + "\tdouble = "+ myDoubleNum + "\tchar = " + myLetter + "\tbool = "+ myBool + "\tString = "+ myText + "\tConstant = "+ constant);
+            Console.WriteLine("Merged text is : " + merged);
+            Console.WriteLine("The e value conversion be like: \n Float 35e3F = "+ f1 + "\n Double 12E4D = "+d1);
 
 
-//             //user input
-//             // Console.WriteLine("Enter your name: ");
-//             // string s2 = Console.ReadLine();
-//             // Console.WriteLine("The input string is : " + s2);
-//             // Console.WriteLine("Enter the num: ");
-//             // int n3 = Convert.ToInt32(Console.ReadLine());
-//             // Console.WriteLine("The input num is : " + n3);
+            //type casting
+            int n1=1;
+            double d2= n1;
+            int n2= (int)d2;//explicit conversion;
+            //string s1="hi";
+            Console.WriteLine(d2);//implicit conversion;;
+            Console.WriteLine(n2);
+            Console.WriteLine(Convert.ToString(n1));
+            Console.WriteLine(Convert.ToInt32(d2));
 
 
-//             //operators
-//             int n4=5;
-//             System.Console.WriteLine(n4&=3);
-//             int n5=5;
-//             System.Console.WriteLine(n5|=3);
-//             int n6=5;
-//             System.Console.WriteLine(n6^=3);
-//             int n7=5;
-//             System.Console.WriteLine(n7>>=3);
-//             int n8=5;
-//             System.Console.WriteLine(n8<<=3);
+            //user input
+            // Console.WriteLine("Enter your name: ");
+            // string s2 = Console.ReadLine();
+            // Console.WriteLine("The input string is : " + s2);
+            // Console.WriteLine("Enter the num: ");
+            // int n3 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("The input num is : " + n3);
 
 
-//             //math operations
-//             System.Console.WriteLine(Math.Max(3,5));
-//             System.Console.WriteLine(Math.Min(3,5));
-//             System.Console.WriteLine(Math.Sqrt(64));
-//             System.Console.WriteLine(Math.Abs(-1.3));
-//             System.Console.WriteLine(Math.Round(9.99));
+            //operators
+            int n4=5;
+            System.Console.WriteLine(n4&=3);
+            int n5=5;
+            System.Console.WriteLine(n5|=3);
+            int n6=5;
+            System.Console.WriteLine(n6^=3);
+            int n7=5;
+            System.Console.WriteLine(n7>>=3);
+            int n8=5;
+            System.Console.WriteLine(n8<<=3);
 
-//             //string
-//             string s3="hey";
-//             string s4="welcome";
-//             string s5 = string.Concat(s3, s4);
-//             Console.WriteLine(s5);
-//             System.Console.WriteLine(s3.Length);
-//             System.Console.WriteLine(s3.ToUpper());
-//             System.Console.WriteLine(s3.ToLower());
 
-//             //string interpolation
-//             string s6 = $"My full name is: {s3} {s4}";
-//             System.Console.WriteLine(s6);
+            //math operations
+            System.Console.WriteLine(Math.Max(3,5));
+            System.Console.WriteLine(Math.Min(3,5));
+            System.Console.WriteLine(Math.Sqrt(64));
+            System.Console.WriteLine(Math.Abs(-1.3));
+            System.Console.WriteLine(Math.Round(9.99));
 
-//             //accessing string
-//             System.Console.WriteLine(s6[5]);
-//             System.Console.WriteLine(s6.IndexOf("f"));
-//             int n9=s6.IndexOf("f");
-//             System.Console.WriteLine(s6.Substring(n9));
+            //string
+            string s3="hey";
+            string s4="welcome";
+            string s5 = string.Concat(s3, s4);
+            Console.WriteLine(s5);
+            System.Console.WriteLine(s3.Length);
+            System.Console.WriteLine(s3.ToUpper());
+            System.Console.WriteLine(s3.ToLower());
 
-//                     }
-//     }
-// }
+            //string interpolation
+            string s6 = $"My full name is: {s3} {s4}";
+            System.Console.WriteLine(s6);
+
+            //accessing string
+            System.Console.WriteLine(s6[5]);
+            System.Console.WriteLine(s6.IndexOf("f"));
+            int n9=s6.IndexOf("f");
+            System.Console.WriteLine(s6.Substring(n9));
+
+                    }
+    }
+}
 
