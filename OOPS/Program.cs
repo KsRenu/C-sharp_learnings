0b7cb0316e76fa04afdcfde660c6c45413e468f1
KsRenu <renuks0802@gmail.com>
2023-01-09 13:13:21 +0530

    implementation of OOPS concepts

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -1,12 +1,96 @@
 ﻿using System;
+using System.IO;
 
 namespace oops
 {
     class program{
+        enum Months{
+                January,    // 0
+                February,   // 1
+                March,      // 2
+                April,      // 3
+                May=10,        // 4
+                June,       // 11
+                July        // 12
+            }
         public static void Main(string[] args){
-            c1 c1_obj= new c1();
-            c1_obj.c1_fun();//calling function of a class from another file
+            c1 c1Obj= new c1();
+            c1Obj.c1Fun();//calling function of a class from another file
+            c2 c2Obj = new c2();
+            c2Obj.c2Fun();
+            c3 c3Obj= new c3("s1");
+            //c4 c4Obj= new c4();
+            //System.Console.WriteLine("s2 from thi class" + c4Obj.s2); will throw error as s2 is private
             
+            Person myObj = new Person();//use short-hand / automatic properties
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+            c6 c6Obj = new c6();
+            c6Obj.c6Func();
+            c6Obj.c5Func();
+            // c7 c7Obj = new c7();
+            // c7Obj.c7Fun();
+            // c8 c8Obj = new c8();
+            // c8Obj.c7Fun();
+            c10 c10Obj = new c10();
+            c10Obj.c9Func();
+            c12 c12Obj= new c12();
+            c12Obj.c11Func1();
+            c12Obj.c11Func2();
+
+            //operations on enum
+            int n1 = (int) Months.April;
+            System.Console.WriteLine(n1);
+            int n2 = (int) Months.June;
+            System.Console.WriteLine(n2);
+
+            Months myVar = Months.April;
+            switch(myVar){
+                case Months.February : 
+                System.Console.WriteLine("Feb");
+                break;
+                case Months.April:
+                System.Console.WriteLine("April");
+                break;
+            }
+
+            //using System.IO needed to access and perform operations on the file system.
+            string writeText = "Hello World!";  
+            File.WriteAllText("file1.txt", writeText); 
+            string readText = File.ReadAllText("file1.txt");  
+            Console.WriteLine(readText);  
+
+
+            //exception
+            try
+            {
+            int[] myNumbers = {1, 2, 3};
+            Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+            Console.WriteLine("Something went wrong.");
+            }
+            finally
+            {
+            Console.WriteLine("The 'try catch' is finished.");
+            }
+
+
+            //throw key word
+            static void checkAge(int age)
+            {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+            }
+
+            checkAge(15);
         }
     }
 }
--- OOPS/Program1.cs
+++ OOPS/Program1.cs
@@ -1,6 +1,92 @@
 class c1{
-        public void c1_fun(){
+        public void c1Fun(){
             //string s1="s1"; //fields of a class
             System.Console.WriteLine("c1");//method pf a class
         }
     }
+
+class c2{
+    public c2(){//A constructor is a special method that is used to initialize objects
+        System.Console.WriteLine("from c2 constructor");
+    }
+    public void c2Fun()
+    {
+        System.Console.WriteLine("c2");
+    }
+}
+
+class c3{
+    public c3(string s1){//constructor with parameters
+        s1=s1;
+        System.Console.WriteLine("The s1 is : " + s1);
+    }
+}
+
+// class c4{
+//     public c4(){
+//         public string s2 = "s2";
+//         System.Console.WriteLine("s2 value from c4");
+//     }
+// }
+
+class Person
+{
+  public string Name  // property
+  { get; set; }
+}
+
+class c5{
+    public void c5Func(){
+        System.Console.WriteLine("From c5 parent");
+    }
+}
+class c6 : c5{
+    public void c6Func()
+    {
+        System.Console.WriteLine("From c6 child");
+    }
+}
+//sealed class Vehicle  //to prevent from other class accessing it
+
+
+// class c7{
+//     public virtual void c7Func(){ //polymorphism 
+//         System.Console.WriteLine("from c7");
+//     }
+// }
+// class c8 : c7{
+//     public override void c7Func(){
+//         System.Console.WriteLine("from c8"); // without virtual in c7Func in c7 class and override in c7Func in c8 class override doesnt function
+//     }
+// }
+
+abstract class c9{
+    public abstract void c9Func();
+    public c9(){
+        System.Console.WriteLine("inside c9");
+    } 
+}
+
+class c10 : c9{
+    public override void c9Func()
+    {
+        System.Console.WriteLine("Inside c10");
+    }
+}
+
+interface Ic11{
+    void c11Func1();
+    void c11Func2();
+
+}
+
+class c12 : Ic11{
+    public void c11Func1(){
+        System.Console.WriteLine("inside c11Func1");
+    }
+    public void c11Func2(){
+        System.Console.WriteLine("inside c11Func2");
+    }
+}
+
+//class DemoClass : IFirstInterface, ISecondInterface    for multiple interfaces
