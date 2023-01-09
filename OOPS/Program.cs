92f3d4b653fd1d176f5d040886bdefd1d1d8e19b
Renu Konda <renuks0802@gmail.com>
2023-01-09 10:38:05 +0530

    implementing OOPS concept

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace oops
+{
+    class program{
+        public static void Main(string[] args){
+            c1 c1_obj= new c1();
+            c1_obj.c1_fun();//calling function of a class from another file
+            
+        }
+    }
+}
---
+++ OOPS/Program1.cs
@@ -0,0 +1,6 @@
+class c1{
+        public void c1_fun(){
+            //string s1="s1"; //fields of a class
+            System.Console.WriteLine("c1");//method pf a class
+        }
+    }
